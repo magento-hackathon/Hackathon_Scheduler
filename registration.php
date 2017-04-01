79fd090f8012b585280d38138704f462d4be0944
Joseph Leedy <joseph@imaginemage.com>
2017-04-01 14:28:19 -0700

    Create extension skeleton

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Hackathon_Scheduler',
+        __DIR__
+    );
